8ee627be58329a4e4b0e4d3ad815bee05e0fc6c4
Mbonigabay <mbonigabay@gmail.com>
2019-09-01 11:08:21 +0200

    feature(view a mentor): Create view mentor api
    [Finishes #168222580]

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -8,9 +8,11 @@ const router = express.Router();
 router.post('/api/v1/auth/signup', usercontroller.Signup);
 router.post('/api/v1/auth/signin', usercontroller.Signin);
 router.get('/api/v1/mentors', usercontroller.ViewAllMentor);
+router.get('/api/v1/mentors/:id', usercontroller.ViewAMentor);
 
 router.post('/api/v1/sessions', sessioncontroller.CreateSession);
 router.patch('/api/v1/sessions/:sessionId/accept', sessioncontroller.AcceptSession);
 router.patch('/api/v1/sessions/:sessionId/reject', sessioncontroller.RejectSession);
+router.post('/api/v1/sessions', sessioncontroller.CreateSession);
 
 export default router;
